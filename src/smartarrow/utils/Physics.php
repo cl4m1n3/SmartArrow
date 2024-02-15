12d9b8936c4376c22cc8c06c538f21326165b71c
cl4m1n3 <116002878+cl4m1n3@users.noreply.github.com>
2024-02-15 23:01:23 +0300

    Add files via upload

--- src/smartarrow/utils/Physics.php
+++ src/smartarrow/utils/Physics.php
@@ -3,32 +3,20 @@ declare(strict_types=1);
 
 namespace smartarrow\utils;
 
-use pocketmine\math\Vector3;
-use pockemtine\block\Air;
-use pocketmine\world\World;
 use pocketmine\entity\Entity;
 use pocketmine\player\Player;
 
 class Physics extends Math
 {
-    public static function objectIsFieldOfView(Entity|Player $entity, Entity|Player $object, int $view): bool
+    public static function objectIsFieldOfView(Player $entity, Player $object, int $view): bool
     {
         $entity_pos = $entity->getPosition();
-        $object_pos = $entity->getPosition();
+        $object_pos = $object->getPosition();
 
-        // VERTICAL
-        $direction = $object_pos->subtractVector($entity_pos)->normalize();
-        $direction_yaw = self::getYaw($direction);
-        $direction_pitch = self::getPitch($direction);
+        $direction1 = $object_pos->subtractVector($entity_pos)->normalize();
+        $direction2 = $entity->getDirectionVector()->normalize();
 
-        // HORIZONTAL
-        $direction = $entity->getDirectionVector()->normalize();
-        $entity_yaw = self::getYaw($direction);
-        $entity_pitch = self::getPitch($direction);
-
-        if (abs($direction_yaw - $entity_yaw) <= $view or abs($direction_pitch - $entity_pitch) <= $view)
-            return true;
-
-        return false;
+        return !self::isOverloaded($direction1, $direction2, $view);
     }
+
 }
